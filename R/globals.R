1d7cc84b1be9911d13d034273608f9860838ade1
Nmoiroux <n.moiroux@gmail.com>
2026-02-05 18:10:54 +0000

    add binded global variables

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,9 @@
 utils::globalVariables(c("lag_end", "lag_start", "value"))
+
+.glmmtmb_family <- c("nbinom2", "nbinom1", "nbinom12", "compois", "truncated_compois",
+                     "genpois", "truncated_genpois", "truncated_poisson", "truncated_nbinom2",
+                     "truncated_nbinom1", "beta_family", "betabinomial", "tweedie", "skewnormal", "lognormal",
+                     "ziGamma", "t_family", "ordbeta", "bell")
+
+.glm_family <- c("gaussian", "binomial", "Gamma",
+                   "inverse.gaussian", "poisson", "quasi", "quasibinomial", "quasipoisson")
