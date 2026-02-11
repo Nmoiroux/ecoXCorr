54870cd4fb2ab1be99238f5cbea3f673c8dc7e6e
Nmoiroux <n.moiroux@gmail.com>
2026-02-11 12:54:54 +0000

    add testthat

---
+++ tests/testthat/test-test.R
@@ -0,0 +1,21 @@
+test_that("aggregate_lagged_intervals returns expected number of lags", {
+  i <- sample(c(1:10), 1)
+  m <- sample(c(1:10), 1)
+  x <-sample(c(1:5), 1)
+
+  res <- aggregate_lagged_intervals(
+    data = meteoMPL2023,
+    date_col = "date",
+    value_cols = "rain_sum",
+    d = unique(albopictusMPL2023$date),
+    i = i,
+    m = m,
+    x = x,
+    funs = list(sum = sum)
+  )
+
+  expect_true(all(c("lag_start", "lag_end") %in% names(res)))
+  expect_equal(as.integer(max(res$end-res$start))+1, i*m)
+  expect_equal(length(unique(albopictusMPL2023$date)) * m * (m+1)/2, nrow(res))
+})
+
